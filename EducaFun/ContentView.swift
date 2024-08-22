6e2d73aedf9faa99281e0d8eb1e6ef6d4a58e3e6
Vikash <arisivikash@gmail.com>
2024-08-22 20:10:00 +0530

    Initial Commit

---
+++ EducaFun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EducaFun
+//
+//  Created by Vikash on 22/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EducaFun/EducaFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducaFunApp.swift
+//  EducaFun
+//
+//  Created by Vikash on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct EducaFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
