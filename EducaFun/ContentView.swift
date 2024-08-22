e5f553d21a2b70efe38ce70c05ac3592786fe381
Vikash <arisivikash@gmail.com>
2024-08-22 21:25:38 +0530

    Complete the design of login and register Views.

--- EducaFun/ContentView.swift
+++ EducaFun/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "heart.fill")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        LoginView()
     }
 }
 
---
+++ EducaFun/RegisterView.swift
@@ -0,0 +1,90 @@
+//
+//  RegisterView.swift
+//  EducaFun
+//
+//  Created by Vikash on 22/08/24.
+//
+
+import SwiftUI
+
+
+import SwiftUI
+
+struct RegisterView: View {
+  @State var name = ""
+  @State var email = ""
+  @State var password = ""
+  var body: some View {
+      ZStack(){
+          Rectangle()
+              .fill(.white)
+          VStack{
+  //The Name, email and password for the newly registered User
+              TextField("Name", text:$name)
+                  .registerpadded()
+              TextField("Email Address", text: $email)
+
+                  .registerpadded()
+                  
+              TextField("Password", text:$password)
+                  .registerpadded()
+              
+
+                  .padding(.bottom, 300)
+          }
+          ZStack{
+             Circle()
+                  .fill(Color(customyellow))
+              .frame(width: 800, height: 700)
+              .offset(x: 50, y: 400)
+              
+              
+              Circle()
+                   .fill(Color(customblue).opacity(0.7))
+               .frame(width: 60, height: 100)
+               .offset(x: -50, y: 3)
+              Circle()
+                   .fill(Color(customblue).opacity(0.7))
+               .frame(width: 90, height: 100)
+               .offset(x: 80, y: 3)
+              Circle()
+                   .fill(Color(customblue).opacity(0.7))
+               .frame(width: 70, height: 100)
+               .offset(x: 10, y: 3)
+          }
+          VStack{
+              Text("Registration")
+                  .foregroundStyle(.black)
+                  .bold()
+                  .font(.title)
+                  .padding(.bottom, 590)
+          }
+      }
+      .ignoresSafeArea()
+  }
+}
+
+#Preview {
+  RegisterView()
+}
+
+struct registerpad: ViewModifier{
+  func body (content: Content) -> some View{
+      content
+          .multilineTextAlignment(.center)
+          .foregroundColor(.black)
+          .font(.system(size: 15))
+          .frame(width:290, height:50)
+          .background(Color(.white))
+          .clipShape(.rect(cornerRadius: 65))
+          .border(Color.black, width:1)
+          .shadow(radius: 0.5)
+          
+      }
+}
+
+extension View{
+  func registerpadded() -> some View{
+      modifier(registerpad())
+  }
+}
