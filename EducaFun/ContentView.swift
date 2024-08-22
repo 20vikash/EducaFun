f4819a7f0033c294042b2c1d717cf0fcdbe19801
Vikash <arisivikash@gmail.com>
2024-08-22 20:41:00 +0530

    My first commit

--- EducaFun/ContentView.swift
+++ EducaFun/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
